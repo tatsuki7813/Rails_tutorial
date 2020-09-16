32f1ec040cf707847dad646accab8642b875375d
ttk <ttk7813@gmail.com>
2020-09-16 23:55:43 +0900

    chapter2-2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,71 +1,48 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @user       = users(:michael)
-    @other_user = users(:archer)
+  setup do
+    @user = users(:one)
   end
 
-  test "should redirect index when not logged in" do
-    get users_path
-    assert_redirected_to login_url
-  end
-
-  test "should get new" do
-    get signup_path
+  test "should get index" do
+    get users_url
     assert_response :success
   end
 
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
+  test "should get new" do
+    get new_user_url
+    assert_response :success
   end
 
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-
-  test "should redirect edit when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-
-  test "should redirect destroy when not logged in" do
-    assert_no_difference 'User.count' do
-      delete user_path(@user)
+  test "should create user" do
+    assert_difference('User.count') do
+      post users_url, params: { user: { email: @user.email, name: @user.name } }
     end
-    assert_redirected_to login_url
+
+    assert_redirected_to user_url(User.last)
   end
 
-  test "should redirect destroy when logged in as a non-admin" do
-    log_in_as(@other_user)
-    assert_no_difference 'User.count' do
-      delete user_path(@user)
+  test "should show user" do
+    get user_url(@user)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_user_url(@user)
+    assert_response :success
+  end
+
+  test "should update user" do
+    patch user_url(@user), params: { user: { email: @user.email, name: @user.name } }
+    assert_redirected_to user_url(@user)
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete user_url(@user)
     end
-    assert_redirected_to root_url
-  end
 
-   test "should redirect following when not logged in" do
-    get following_user_path(@user)
-    assert_redirected_to login_url
-  end
-
-  test "should redirect followers when not logged in" do
-    get followers_user_path(@user)
-    assert_redirected_to login_url
+    assert_redirected_to users_url
   end
 end
